1efeaf071cded2911de479592ccc65ce2400c1aa
Dmitrii Raven <oguvan4ik@gmail.com>
2021-03-19 14:09:21 +0200

    added class Contact

---
+++ Practicce_3_1/Contact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practicce_3_1
+{
+    public class Contact
+    {
+        public Contact(string firstName, string lastName, string fullName, string number)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            FullName = fullName;
+            Number = number;
+        }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Number { get; set; }
+    }
+}
